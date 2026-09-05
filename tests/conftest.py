e047e2e8f57829940ec00eec1471ddfc7eb80ca2
NP <polyakovanadezhda@yandex.ru>
2026-09-05 22:33:29 +0200

    Home_work Selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.common.options import ArgOptions
 
 from pages.currency_selector import CurrencySelector
 
@@ -15,7 +16,13 @@ load_dotenv()
 
 def pytest_addoption(parser):
     parser.addoption("--browser", default="chrome")
+    parser.addoption("--browser_version", default="128.0")
     parser.addoption("--base_url", default="http://localhost:8081/")
+    parser.addoption(
+        "--executor",
+        default="local",
+        help="local — браузер на этой машине, иначе имя хоста с WebDriver (например, ggr)",
+    )
 
 
 @pytest.hookimpl(hookwrapper=True)
@@ -36,10 +43,21 @@ def pytest_runtest_makereport(item, call):
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name: str = request.config.getoption("--browser")
+    browser_version: str = request.config.getoption("--browser_version")
+    executor: str = request.config.getoption("--executor")
 
     headless = os.getenv("HEADLESS", "").lower() in ("1", "true", "yes")
 
-    if browser_name == "chrome":
+    if executor != "local":
+        options = ArgOptions()
+        options.set_capability("browserName", browser_name)
+        options.set_capability("browserVersion", browser_version)
+        options.set_capability("selenoid:options", {"enableVNC": True})
+        browser = webdriver.Remote(
+            command_executor=f"http://{executor}:4444/wd/hub",
+            options=options,
+        )
+    elif browser_name == "chrome":
         options = ChromeOptions()
         if headless:
             options.add_argument("--headless=new")
